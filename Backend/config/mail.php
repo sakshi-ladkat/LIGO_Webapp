a1c6df372a7126e5bec1416bdb46b935220345cd
Sakshi-ladkat <ladkatsakshi2507@gmail.com>
2026-05-15 14:17:31 +0530

    MAIL Domain  updated

--- Backend/config/mail.php
+++ Backend/config/mail.php
@@ -47,7 +47,7 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', ''), PHP_URL_HOST)),
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST) ?: 'localhost'),
         ],
 
         'ses' => [
